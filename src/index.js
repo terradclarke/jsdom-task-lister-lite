6a4912c28575a88f23211b68db1faffa9db0d4ed
Terradclarke <terranie67@gmail.com>
2020-11-19 20:51:12 -0600

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  handleForm()
 });
+
+const handleForm = () => {
+
+const taskForm = document.querySelector('#create-task-form')
+
+taskForm.addEventListener('submit',(event) => {
+  event.preventDefault()
+
+  const taskInput = event.target.querySelector('#new-task-description')
+
+    const taskLi = document.createElement('li')
+
+    taskLi.innerText = taskInput.value 
+
+    const tasksParrent = document.querySelector('#list')
+    tasksParrent.appendChild(taskLi)
+
+    event.target.reset()
+})
+
+}
+
+
+// const main = document.querySelector('#main-content')
+// console.log(main)
+
+// const taskForm = document.querySelector('#create-task-form')
+// const tasklist = document.querySelector('#tasks')
+
+// taskForm.addEventListener("submit", function(e) {
+//   e.preventDefault()
+
+//   const newTask = document.querySelector('#new-task-description').value 
+
+//   tasklist.innerHTML += 
+//     <li> ${newTask}
+//       <button> X for delete </button> 
+//     </li>
+
+//     taskForm.reset()
+// })
+
+// tasklist.addEventListener("click", function(e) {
+//   console.log(e.target)
+//   if (e.target.database.action === "delete" {
+//     e.target.parentElement.remove 
+//   }
+// }
